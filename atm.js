88689440bb1832b064aa5157ce86a825223ca2f5
David James <d.james88@btinternet.com>
2015-05-06 19:13:03 +0100

    withdraw 1

--- atm.js
+++ atm.js
@@ -1,13 +1,26 @@
 $(document).ready(function(){
   var balance;
+  var withdraw;
 
- // deposits #amount1 to #balance1 but isnt storing
+ // deposits #amount1 to #balance1 & is stored in var balance
    $('#deposit1').click(function(){
    balance = $('#amount1').val();
    $('#balance1').html(balance);
-   //return this.balance;
-   debugger;
+   //debugger;
+   
+   //withdraws amount and updates balance
+   $('#withdraw1').click(function(){
+    withdraw = $('#amount1').val();
+    balance = balance - withdraw;
+    $('#balance1').html(balance);
+    debugger;
+   })
     })
+
+
+
+
+
 
 // // deposits #amount2 to #balance2 but isnt storing
 //    $('#deposit2').click(function(){
