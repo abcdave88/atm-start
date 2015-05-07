424899ab64878c271597f4d77fd749b84099ed86
David James <d.james88@btinternet.com>
2015-05-07 08:39:14 +0100

    finshed but not finished

--- atm.js
+++ atm.js
@@ -4,47 +4,45 @@ $(document).ready(function(){
   var savBalance;
   var savWithdraw;
 
- // deposits #amount1 to #balance1 & is stored in var balance
-   $('#deposit1').click(function(){
-   balance = $('#amount1').val();
-   $('#balance1').html(balance);
-   //debugger;
+   // deposits #amount1 to #balance1 & is stored in var balance
+       $('#deposit1').click(function(){
+       balance = $('#amount1').val();
+       $('#balance1').html(balance);
+       //debugger;
+     })
+     //withdraws amount and updates balance
+       $('#withdraw1').click(function(){
+        withdraw = $('#amount1').val();
+        balance = balance - withdraw;
+        debugger;
+        $('#balance1').html(balance); //takes double from savings, needs another variable.
+        if (withdraw > balance) {
+          var diff = (withdraw - balance);
+          debugger;
+          $('#balance2').html(savBalance - diff);
+          $('#balance1').html(0);
+        debugger;
+        }
+        
    
-   //withdraws amount and updates balance
-   $('#withdraw1').click(function(){
-    withdraw = $('#amount1').val();
-    balance = balance - withdraw;
-    $('#balance1').html(balance);
-    debugger;
-   })
-    })
+ })
+       //deposits into savings
+       $('#deposit2').click(function(){
+       savBalance = $('#amount2').val();
+       $('#balance2').html(savBalance);
+       //debugger;
+       
+      })  
+       //withdraws amount and updates savings
+       $('#withdraw2').click(function(){
+        savWithdraw = $('#amount2').val();
+        savBalance = savBalance - savWithdraw;
+        $('#balance2').html(savBalance);
+        //debugger;
+  
+ })
+});
 
-   $('#deposit2').click(function(){
-   savBalance = $('#amount2').val();
-   $('#balance2').html(savBalance);
-   //debugger;
-   
-   //withdraws amount and updates balance
-   $('#withdraw2').click(function(){
-    savWithdraw = $('#amount2').val();
-    savBalance = savBalance - savWithdraw;
-    $('#balance2').html(savBalance);
-    debugger;
-   })
-    })
-
-
-
-
-// // deposits #amount2 to #balance2 but isnt storing
-//    $('#deposit2').click(function(){
-//    $('#balance2').html($('#amount2').val());
-//     balance = $('#amount2');
-//    debugger;
-//     })
-
-   });
-//});
 
 
 
